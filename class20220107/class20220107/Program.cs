a93a3f056cd4717329e3575326ea7270e4e61405
Bob yu <y9y9j8@163.com>
2022-01-09 14:45:31 -0600

    完成part2

--- class20220107/class20220107/Program.cs
+++ class20220107/class20220107/Program.cs
@@ -13,20 +13,40 @@ namespace class20220107
             //create an array
             int[] arr = new int[3];
 
-            //read the value of this array from the user
-            for (int i = 0; i < arr.Length; i++)
-            {
-                Console.WriteLine($"please enter the {i} value");
-                arr[i] = int.Parse(Console.ReadLine());
-            }
-            PrintIntgArray(arr);
+            ////read the value of this array from the user
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.WriteLine($"please enter the {i} value");
+            //    arr[i] = int.Parse(Console.ReadLine());
+            //}
+            //PrintIntgArray(arr);
+            ////1---
+            //string[] strArray = new string[] { "1", "2", "3" };
+            //PrintStirngArray(strArray);
 
-            string[] strArray = new string[] { "1", "2", "3" };
-            PrintStirngArray(strArray);
+            //2---
+            int[] intArray = new int[] { 2, 4, 5, 2, 8, 2 };
+            Console.WriteLine("please enter a number");
+            int num = int.Parse(Console.ReadLine());
+            int countNumber = CountNumberInArray(intArray, num);
+            Console.WriteLine(countNumber);
         }
 
+        //2---write a method to return the frequency of a number in an interger array
+        //{2,4,5,2,8,2} freq(2) =3
+        static int CountNumberInArray(int[] intArray,int freq)
+        {
+            int countSum = 0;
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                if (intArray[i] == freq)
+                    countSum++;
+            }
+            return countSum;
 
-        //write a method to print a String Array
+        }
+
+        //1---write a method to print a String Array
         static void PrintStirngArray(string[] strArray)
         {
             for(int i=0;i< strArray.Length;i++)

d4f78c3f762f2d1b74a2fb4c304e146f29e40580
Bob yu <y9y9j8@163.com>
2022-01-09 21:39:51 -0600

    第一个项目，写了一个钢琴键盘

---
+++ 钢琴曲/钢琴曲/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WMPLib;
+
+namespace 钢琴曲
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (1 == 1)
+            {
+                ConsoleKeyInfo ck = Console.ReadKey(true);
+                char c = ck.KeyChar;
+                Console.WriteLine(c);
+                //Console.WriteLine(ck.KeyChar);
+                WindowsMediaPlayer wm = new WindowsMediaPlayer();
+                wm.URL = @"E:\video\" + c + ".mp3";
+
+                //Console.Beep(800, 500);
+            }
+        }
+    }
+}

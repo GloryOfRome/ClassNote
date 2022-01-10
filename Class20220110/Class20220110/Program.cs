04a8e3b98b43e9139df0d048b8c393ee0172ff10
Bob yu <y9y9j8@163.com>
2022-01-10 10:10:47 -0600

    将数组重复数字去除

--- Class20220110/Class20220110/Program.cs
+++ Class20220110/Class20220110/Program.cs
@@ -10,6 +10,96 @@ namespace Class20220110
     {
         static void Main(string[] args)
         {
+            int[] intArray1 = new int[] { 1, 3, 1, 4, 5, 5, 6 };
+            FindTwoNumber(intArray1);
+
+            Console.WriteLine("-------------------");
+            int[] intArray2 = new int[] { 1, 1, 3, 1, 4, 5, 5, 6 };
+            int[] temporaryNumArray = BubbleSort(intArray2);
+            everyEleArray(temporaryNumArray);
+
+            Console.WriteLine("-------------------");
+            FindOverTwoNumber(intArray2);
+
+            ////有问题
+            //Console.WriteLine("-------------------");
+            ////把array装入list数组
+            //List<int> li = new List<int>();
+            //for (int i = 0; i < temporaryNumArray.Length; i++)
+            //{
+            //    if(temporaryNumArray[i]== temporaryNumArray[i+1])
+            //        li.Add(temporaryNumArray[i]);
+            //}
+
+            //Console.WriteLine("-------------------");
+            ////everyEleArray(li);
+            //foreach (var el in li)
+            //    Console.WriteLine(el);
+        }
+        //遍历List
+        static void everyEleArray(List<int> newList )
+        {
+            foreach(var el in newList)
+                Console.WriteLine(el);
+        }
+
+        //遍历数组
+        static void everyEleArray(int[] intArray)
+        {
+            for (int i = 0; i < intArray.Length; i++)
+                Console.WriteLine(intArray[i]);
+        }
+
+        //对数组进行排序
+        static int[] BubbleSort(int[] intArray)
+        {
+            int temporaryNum = 0;
+            for (int i = 0; i < intArray.Length - 1; i++)
+            {
+                for (int j = i + 1; j < intArray.Length; j++)
+                {
+                    if (intArray[i] > intArray[j])
+                    {
+                        temporaryNum = intArray[i];
+                        intArray[i] = intArray[j];
+                        intArray[j] = temporaryNum;
+                    }
+                }
+            }
+            return intArray;
+        }
+
+        //如果重复数字只有两个可以实现
+        static void FindTwoNumber(int[] intArray)
+        {
+            for (int i = 0; i < intArray.Length - 1; i++)
+            {
+                for (int j = i + 1; j < intArray.Length; j++)
+                {
+                    if (intArray[i] == intArray[j])
+                    {
+                        Console.WriteLine(intArray[i]);
+                    }
+                }
+            }
+        }
+
+        //如果重复数字超过两个可以实现
+        static void FindOverTwoNumber(int[] intArray)
+        {
+            if(intArray[0] != intArray[1])
+                Console.WriteLine(intArray[0]);
+            Console.WriteLine("-------------------");
+
+            for (int i = 1; i < intArray.Length - 1; i++)
+            {
+                if (intArray[i] != intArray[i - 1] && intArray[i] != intArray[i+1])
+                   Console.WriteLine(intArray[i]);
+            }
+            Console.WriteLine("-------------------");
+
+            if (intArray[intArray.Length-1] != intArray[intArray.Length-2])
+                Console.WriteLine(intArray[intArray.Length - 1]);
         }
     }
 }
